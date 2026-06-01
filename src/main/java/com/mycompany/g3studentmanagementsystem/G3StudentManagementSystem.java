03d2b7390e29292a37bbad490a9bb5589ddbffac
santosdacsallen <santos.dacsallen@gmail.com>
2026-06-02 02:48:14 +0800

    Update G3StudentManagementSystem.java
    test gui

--- src/main/java/com/mycompany/g3studentmanagementsystem/G3StudentManagementSystem.java
+++ src/main/java/com/mycompany/g3studentmanagementsystem/G3StudentManagementSystem.java
@@ -7,6 +7,6 @@ public class G3StudentManagementSystem{
     public static void main(String[] args) {
 		LandingPageGUI lp = new LandingPageGUI();
 		lp.setVisible(true);
-        
+                
     }
 }
